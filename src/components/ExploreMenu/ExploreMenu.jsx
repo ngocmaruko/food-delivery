6192397026c69e179cdca5bff24cb048a572a9e1
Ngoc <ngoc.held9@gmail.com>
2024-07-02 17:22:11 +0900

    menu items

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+export const ExploreMenu = () => {
+  return (
+    <div className='explore-menu'>
+
+    </div>
+  )
+}
