2e653243d7bfe20a6d8e1c232a5f0ea6e47b4933
Ngoc <ngoc.held9@gmail.com>
2024-07-03 17:33:50 +0900

    toggle

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-export const ExploreMenu = () => {
+export const ExploreMenu = ({category, setCategory}) => {
   return (
-    <div className='explore-menu'>
-
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">Choose from a diverse menu featurning a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <div className="explore-menu-list">
+        {(menu_list).map((item, i) => (
+          <div key={i} className='explore-menu-list-item'>
+            <img 
+            className={category?'active':''} 
+            onClick={() => setCategory(!category)}
+            src={item.menu_image} alt={item.menu_name} />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
     </div>
   )
 }
