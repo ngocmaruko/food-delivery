0c2daa59831d8a95b15b4681822274e9992c0dae
Ngoc <ngoc.held9@gmail.com>
2024-07-05 16:51:04 +0900

    context API

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,10 +9,9 @@ export const ExploreMenu = ({category, setCategory}) => {
       <p className="explore-menu-text">Choose from a diverse menu featurning a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
       <div className="explore-menu-list">
         {(menu_list).map((item, i) => (
-          <div key={i} className='explore-menu-list-item'>
+          <div onClick={() => setCategory(prev => prev === item.menu_name ? 'All' : item.menu_name)} key={i} className='explore-menu-list-item'>
             <img 
-            className={category?'active':''} 
-            onClick={() => setCategory(!category)}
+            className={category === item.menu_name?'active':''} 
             src={item.menu_image} alt={item.menu_name} />
             <p>{item.menu_name}</p>
           </div>
