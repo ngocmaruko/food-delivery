0628860f79efa6a315a12729ef8689ff7c6f3712
Ngoc <ngoc.held9@gmail.com>
2024-07-12 17:25:13 +0900

    cart object

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext';
 
 export const FoodItem = ({id,name,price,description,image}) => {
   const [itemCount, setItemCount] = useState(0);
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img src={image} alt={name} className="food-item-image" />
-        {!itemCount
-          ? <img className='add' onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_white}/>
+        {!cartItems[id]
+          ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white}/>
           : <div className='food-item-counter'>
-              <img onClick={() => setItemCount(prev => prev-1)} src={assets.remove_icon_red} />
-              <p>{itemCount}</p>
-              <img onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_green} />
+              <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} />
+              <p>{cartItems[id]}</p>
+              <img onClick={() => addToCart(id)} src={assets.add_icon_green} />
           </div>
         }
       </div>
       <div className="food-item-info">
           <div className="food-item-name-rating">
-            <p>{name}</p>
+            <p>{name}</p>  
             <img src={assets.rating_starts} alt="" />
           </div>
           <p className="food-item-desc">{description}</p>
